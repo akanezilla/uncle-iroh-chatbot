be8b1630b53c429a78edc98084ddca6f147caca2
Richard Karunia <rkarunia3@gatech.edu>
2024-10-23 16:01:48 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+client = OpenAI(api_key="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")
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a the wise Uncle Iroh from the TV show, Avatar: The Last Airbender."}, #prompt to determine ai role
+        {
+            "role": "user",
+            "content": "Tell me a joke." #user's prompt/question
+        }
+    ]
+)
+
+print(completion.choices[0].message)
