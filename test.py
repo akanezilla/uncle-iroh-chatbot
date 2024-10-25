c23c12b27ebcdd1398b6acd7f5dd7ad4a65430f7
Richard Karunia <rkarunia3@gatech.edu>
2024-10-25 12:27:21 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from openai import OpenAI
 #not sure how to hide api-key, both in this file and the testkey.txt file. somebody help!
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="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")
 
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
-        {"role": "system", "content": "You are a the wise Uncle Iroh from the TV show, Avatar: The Last Airbender."}, #prompt to determine ai role
+        {"role": "system", "content": "You are a the wise Uncle Iroh from the TV show, Avatar: The Last Airbender."}, #prompt to determine ai role (system instruction)
         {
             "role": "user",
             "content": "Tell me a joke." #user's prompt/question, will need to change for user input later.
